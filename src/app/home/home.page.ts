4339d162a8d1f536b5b3a086e6ffe76adb2b0a5a
unknown <bylasuhaimi@gmail.com>
2020-08-01 20:47:09 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,141 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CalendarComponent } from 'ionic2-calendar';
+import { CalModalPage } from '../cal-modal/cal-modal.page';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  eventSource= []; //event that would be displayed in the code
+  viewTitle: string; //month/ week/ daily (depends)
 
-  constructor() {}
+  calendar = {
+    mode: 'month',
+    currentDate: new Date(),
+  }
 
+ // selectedDate: Date;
+
+  @ViewChild(CalendarComponent) myCal: CalendarComponent
+  constructor(private modalCtrl: ModalController) {}
+
+  ngOnInit() {}
+
+  next(){
+    this.myCal.slideNext();
+  }
+  back(){
+    this.myCal.slidePrev();
+  }
+//Selected date reange and hence title changed
+  onViewTitleChanged(title){
+    this.viewTitle=title;
+  }
+
+  
+ 
+  createRandomEvents() {
+    var events = [];
+    for (var i = 0; i < 50; i += 1) {
+      var date = new Date();
+      var eventType = Math.floor(Math.random() * 2);
+      var startDay = Math.floor(Math.random() * 90) - 45;
+      var endDay = Math.floor(Math.random() * 2) + startDay;
+      var startTime;
+      var endTime;
+      if (eventType === 0) {
+        startTime = new Date(
+          Date.UTC(
+            date.getUTCFullYear(),
+            date.getUTCMonth(),
+            date.getUTCDate() + startDay
+          )
+        );
+        if (endDay === startDay) {
+          endDay += 1;
+        }
+        endTime = new Date(
+          Date.UTC(
+            date.getUTCFullYear(),
+            date.getUTCMonth(),
+            date.getUTCDate() + endDay
+          )
+        );
+        events.push({
+          title: 'All Day - ' + i,
+          startTime: startTime,
+          endTime: endTime,
+          allDay: true,
+        });
+      } else {
+        var startMinute = Math.floor(Math.random() * 24 * 60);
+        var endMinute = Math.floor(Math.random() * 180) + startMinute;
+        startTime = new Date(
+          date.getFullYear(),
+          date.getMonth(),
+          date.getDate() + startDay,
+          0,
+          date.getMinutes() + startMinute
+        );
+        endTime = new Date(
+          date.getFullYear(),
+          date.getMonth(),
+          date.getDate() + endDay,
+          0,
+          date.getMinutes() + endMinute
+        );
+        events.push({
+          title: 'Event - ' + i,
+          startTime: startTime,
+          endTime: endTime,
+          allDay: false,
+        });
+      }
+    }
+    this.eventSource = events; //to sending your event source to events
+  }
+ 
+  removeEvents() {
+    this.eventSource = [];
+  }
+  async openCalModal() {
+    const modal = await this.modalCtrl.create({
+      component: CalModalPage,
+      cssClass: 'cal-modal',
+      backdropDismiss: false
+    });
+   
+    await modal.present();
+   //he onDidDismiss event, in which we
+   //might get back a new event that we then add to our eventSource
+    modal.onDidDismiss().then((result) => {
+      if (result.data && result.data.event) {
+        let event = result.data.event;
+        if (event.allDay) {
+          let start = event.startTime;
+          event.startTime = new Date(
+            Date.UTC(
+              start.getUTCFullYear(),
+              start.getUTCMonth(),
+              start.getUTCDate()
+            )
+          );
+          event.endTime = new Date(
+            Date.UTC(
+              start.getUTCFullYear(),
+              start.getUTCMonth(),
+              start.getUTCDate() + 1
+            )
+          );
+        }
+        this.eventSource.push(result.data.event);
+        this.myCal.loadEvents();
+        //we also need to call loadEvents() on the calendar 
+        //component in order to update the data!
+      }
+    });
+  }
 }
